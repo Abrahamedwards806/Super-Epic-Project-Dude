0baa258857351d16203b91a63f56d34ca3f6596e
Abraham Edwards <aedwards@cambridge.local>
2023-03-08 15:28:37 -0500

    initial

---
+++ src/me/abe/sepd/Main.java
@@ -0,0 +1,12 @@
+package me.abe.sepd;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Blah");
+		System.out.println("value is " + add(7, 7));
+		
+	}
+	public static int add(int pie, int cheese) {
+		return pie + cheese; 
+	}
+}
